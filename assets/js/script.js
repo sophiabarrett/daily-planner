d22d8032a8942e33c4804e07fe8a33dd54af5518
Sophia Barrett <sophia@yoursummit.media>
2021-10-08 14:09:24 -0700

    color code time blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,8 @@ for (var hour = 9; hour <= 17; hour++) {
         hour: moment().set({
             "hour": hour,
             "minute": "0",
-            "second": "0"
+            "second": "0",
+            "millisecond": "0"
         }).format("hA"),
         plan: ""
     };
@@ -19,18 +20,18 @@ for (var hour = 9; hour <= 17; hour++) {
 
 console.log(plannerArr);
 
-// loop through planner array and create time block for each object
+// loop through planner array and create bootstrap row & columns for each object
 for (var i = 0; i < plannerArr.length; i++) {
     // create column for time
     var timeColEl = $("<div>")
         .addClass("col-1 hour")
         .text(plannerArr[i].hour);
-    //create column for textarea
+    // create column for textarea
     var textColEl = $("<textarea>")
-        .addClass("col-text col-10");
+        .addClass("col-10");
     // create column for save button
     var saveColEl = $("<button>")
-        .addClass("col-save col-1 btn saveBtn");
+        .addClass("col-1 btn saveBtn");
     //create row and add all columns to it
     var rowEl = $("<div>")
         .addClass("row time-block")
@@ -42,13 +43,31 @@ for (var i = 0; i < plannerArr.length; i++) {
 }
 
 // color code time blocks based on current time
-var auditTimeBlocks = function() {
-    // get current time
-    var currentTime = moment();
+var auditTimeBlocks = function () {
+    // get current hour (instead of exact time) as moment.js object
+    var currentHour = moment()
+        .set({
+            "minute": "0",
+            "second": "0",
+            "millisecond": "0"
+        });
 
-    $(".time-block").each(function() {
-        console.log($(this).children(".hour").text());
-        console.log($(this).children("textarea"));
+    $(".time-block").each(function () {
+        // remove previously set color coding classes
+        $(this).children("textarea").removeClass("past present future")
+
+        // get each time block's displayed time and convert to moment.js object
+        var timeColElText = $(this).children(".hour").text();
+        var blockTime = moment(timeColElText, "hA");
+
+        // compare each time block to current hour and add appropriate class
+        if (blockTime.isBefore(currentHour)) {
+            $(this).children("textarea").addClass("past");
+        } else if (blockTime.isSame(currentHour)) {
+            $(this).children("textarea").addClass("present");
+        } else {
+            $(this).children("textarea").addClass("future");
+        }
     });
 }
 
